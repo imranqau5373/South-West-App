f5b8d158dc9e3d3582045a473a0bb733888850b6
Muhammad ibrahim <Muhammadibrahimtanoli@outlook.com>
2022-06-08 17:10:36 +0500

    Changes Added

--- src/app/core/components/add-patient-one/add-patient-one.component.ts
+++ src/app/core/components/add-patient-one/add-patient-one.component.ts
@@ -39,7 +39,7 @@ export class AddPatientOneComponent implements OnInit {
     this.patientOneModel.headche = "No";
     this.patientOneModel.vomiting = "No";
     this.patientOneModel.bodyache = "No";
-    this.patientOneModel.apt = "Apartment";
+    this.patientOneModel.apt = "Apartment";  
   }
 
   nextClick(){
